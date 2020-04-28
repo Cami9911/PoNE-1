bad3d8d8065bd1bebd93b9027c83b7ade7b1cfff
DianaTerchea <44743846+DianaTerchea@users.noreply.github.com>
2020-04-28 14:50:47 +0300

    GenerateExercie Backend Done

---
+++ database.js
@@ -0,0 +1,62 @@
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host: 'polish-notation-server.mysql.database.azure.com',
+    user: 'Kavarna@polish-notation-server',
+    password: '!Parolamea123',
+    database: 'PolishNotationDatabase',
+    port: 3306,
+    ssl: true
+});
+
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+//populateTable();
+selectFromTable();
+
+function populateTable() {
+    var exercises = ["_ * ( ( ( _ + _ ) + ( _ * _ ) ) - _ )", "_ + _ + ( _ * _ ) - ( _ / _ )", "((_+_)?_)-_"];
+    var i;
+
+    for (i = 0; i < exercises.length; i++) {
+        insertIntoTable(exercises[i]);
+        commitChanges();
+    }
+};
+
+function insertIntoTable(exercise) {
+    conn.query("INSERT INTO exercises (exercise) VALUES (?);", (String(exercise)), function(err, result, fields) {
+        if (err) throw err;
+        console.log('Inserted ' + result.affectedRows + ' row(s).');
+        console.log('Insert successfull...');
+    });
+}
+
+function commitChanges() {
+    conn.query("Commit;", function(err, result, fields) {
+        if (err) throw err;
+        console.log('Commit successfull...');
+    });
+};
+
+function deleteFromTable() {
+    conn.query("Delete from exercises where id_exercise >= 1 and id_exercise <= 10;", function(err, result, fields) {
+        if (err) throw err;
+        console.log('Delete Successfull...');
+    });
+}
+
+
+function selectFromTable() {
+    conn.query("Select * from exercises;", function(err, result, fields) {
+        if (err) throw err;
+        var i;
+        for (i = 0; i < result.length; i++) {
+            console.log(result[i].id_exercise);
+            console.log(result[i].exercise)
+        }
+    });
+};
---
+++ src/exerciseScript.js
@@ -0,0 +1,28 @@
+function generateExercise() {
+    var displayed = 0;
+    var xhttp;
+
+    if (window.XMLHttpRequest) {
+        // code for modern browsers
+        xhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhttp.onreadystatechange = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            console.log(response);
+        } else {
+            console.log(response);
+            console.log('eroare de aia mare');
+        }
+    };
+
+    xhttp.open("POST", "./exercise.js", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(null);
+
+}

e5050fcf4f40fbe18e2a8fe5927d2b6d81549170
Dobrica Stefan <sdobrica188@gmail.com>
2020-05-16 00:14:25 +0300

    Two rows text

--- src/MainText.js
+++ src/MainText.js
@@ -1,9 +1,13 @@
 "use strict"
 
-class MainText extends SVGText {
+class MainText {
 
     constructor(renderer, position, fontSize, fontColor, text, animationTime = 1.5) {
-        super(renderer, position, fontSize, fontColor, "", "explain-text");
+        this.text = new SVGText(renderer, position, fontSize, fontColor, "", "explain-text");
+        this.text2 = new SVGText(renderer, [position[0], position[1] + fontSize + 10], fontSize, fontColor, "", "explain-text");
+
+        this.fontSize = fontSize;
+        this.renderer = renderer;
         this.animationTime = animationTime;
         this.jobs = [];
         this.position = position;
@@ -25,30 +29,57 @@ class MainText extends SVGText {
         this.addJob(dt => {
             waitTime -= dt;
             if (waitTime <= 0.0) {
-                this.setAnimation('fade-out', this.animationTime);
+                this.text.setAnimation('fade-out', this.animationTime);
+                this.text2.setAnimation('fade-out', this.animationTime);
                 return false;
             } else {
                 return true;
             }
         });
         this.addSeparator();
-        this.addJob(dt => super.update(dt));
+        this.addJob(dt => this.text.update(dt));
+        this.addJob(dt => this.text2.update(dt));
     }
 
     fadeInText(text) {
         this.addSeparator();
         this.addJob(dt => {
-            this.setText(text);
-            this.setPosition(this.position[0], this.position[1]);
-            this.setAnimation('fade-in', this.animationTime);
+            let firstText = "";
+            let secondText = "";
+            let lastSpace = 0;
+            for (let i = 0; i < text.length; ++i) {
+                if (text[i] === " ") {
+                    lastSpace = i;
+                }
+                if (i > 50) {
+                    firstText = text.substr(0, lastSpace);
+                    secondText = text.substr(lastSpace, text.length);
+                }
+            }
+            if (firstText === "") {
+                firstText = text;
+            }
+            this.text.setText(firstText);
+            this.text.setPosition(this.position[0], this.position[1]);
+            this.text.setAnimation('fade-in', this.animationTime);
+            this.text2.setText(secondText);
+            this.text2.setPosition(this.position[0], this.position[1] + this.fontSize + 10);
+            this.text2.setAnimation('fade-in', this.animationTime);
             return false;
         });
         this.addSeparator();
-        this.addJob(dt => super.update(dt));
+        this.addJob(dt => this.text.update(dt));
+        this.addJob(dt => this.text2.update(dt));
     }
 
     update(dt) {
         return this.renderer.executeJobs(this.jobs, dt);
+    }
+
+    render(projectionMatrix) {
+        this.text.render(projectionMatrix);
+        this.text2.render(projectionMatrix);
+        return true;
     }
 
 }
